a54831dfe968e0f28fc40b308da15f04c112c28e
yiannisha <hadjiyiannis.3@gmail.com>
2024-10-03 07:35:55 +0300

    Add exercise 1 kernels

--- chapter3/matmul/matmul.cu
+++ chapter3/matmul/matmul.cu
@@ -32,6 +32,65 @@ void matMulKernel(
     out[row * K + col] = outVal;
 }
 
+// Exercise 1 (a)
+__global__
+void matMulKernel_exer1_a(
+    int* left,
+    int* right,
+    int* out,
+    uint M,
+    uint N,
+    uint K
+)
+{
+    // row is constant since each thread is used to calculate a row of the result
+    uint row = blockDim.y * blockIdx.y + threadIdx.y;
+    // col is always zero
+    // left this comment for better understanding
+    // uint col = 0;
+
+    if (row >= M) return;
+
+    // col is "moving" on the x axis
+    for (int i=0; i<K; i++) {
+        // left this comment for better understanding - it would replace all `i` instances used in this loop
+        // uint currCol = col + i;
+        int val = 0;
+
+        for (int k=0; k<N; k++) {
+            val += left[row * N + k] * right[k * K + i];
+        }
+        out[row * K + i] = val;
+    }
+
+}
+
+// Exercise 1 (b)
+__global__
+void matMulKernel_exer1_b(
+    int* left,
+    int* right,
+    int* out,
+    uint M,
+    uint N,
+    uint K
+)
+{
+    // uint row = 0;
+    uint col = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (col >= K) return;
+
+    for (int i=0; i<M; i++) {
+        int val = 0;
+
+        for (int k=0; k<N; k++) {
+            val += left[i * N + k] * right[k * K + col];
+        }
+        out[i * K + col] = val;
+    }
+}
+
 int main()
 {
     int length = 7;
@@ -73,12 +132,23 @@ int main()
     cudaMemcpy(A_d, A, length_A*sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(B_d, B, length_B*sizeof(int), cudaMemcpyHostToDevice);
 
-    // it is convenient to map 2D data to a 2D grid consisting of
-    // 2D blocks
+    // it is convenient to map 2D data to a 2D grid consisting of 2D blocks
     dim3 dimGrid = dim3(ceil(K/(float)32), ceil(M/(float)32), 1);
     dim3 dimBlocks = dim3(32, 32, 1);
 
     matMulKernel<<<dimGrid, dimBlocks>>>(A_d, B_d, out_d, M, length, K);
+
+    // Exercise 1 (a)
+    // dim3 dimGrid = dim3(1, ceil(M/(float)32), 1);
+    // dim3 dimBlocks = dim3(1, 32, 1);
+
+    // matMulKernel_exer1_a<<<dimGrid, dimBlocks>>>(A_d, B_d, out_d, M, length, K);
+
+    // Exercise 1 (b)
+    // dim3 dimGrid = dim3(ceil(M/(float)32), 1, 1);
+    // dim3 dimBlocks = dim3(32, 1, 1);
+
+    // matMulKernel_exer1_b<<<dimGrid, dimBlocks>>>(A_d, B_d, out_d, M, length, K);
 
     cudaMemcpy(out_h, out_d, size, cudaMemcpyDeviceToHost);
 
